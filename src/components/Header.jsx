56efd3b1e4e4595265014a5bc432a994ddf6a635
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-14 16:02:26 +0200

    add desktop version of header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import classes from "./Header.module.css";
+export default function Header() {
+  return (
+    <header className={classes.header}>
+      <img
+        className={classes.logo}
+        src="src/assets/shared/desktop/logo-dark.png"
+        alt=""
+      />
+      <img
+        className={classes.hamburger}
+        src="src/assets/shared/mobile/icon-hamburger.svg"
+        alt=""
+      />
+    </header>
+  );
+}

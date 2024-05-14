ee327cf160920bf447c477dade781229772aafea
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-14 20:13:57 +0200

    finish basic styling of first section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,23 @@
+import Button from "../components/UI/Button";
 import classes from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Award-winning custom designs and digital branding solutions</h1>
-      <p>
-        With over 10 years in the industry, we are experienced in creating fully
-        responsive websites, app design, and engaging brand experiences. Find
-        out more about our services.
-      </p>
-      <button>Learn more</button>
-      <div className={classes.phone} />
-    </div>
+    <section className={classes.intro}>
+      <div className={classes["intro-text"]}>
+        <h1>Award-winning custom designs and digital branding solutions</h1>
+        <p>
+          With over 10 years in the industry, we are experienced in creating
+          fully responsive websites, app design, and engaging brand experiences.
+          Find out more about our services.
+        </p>
+        <Button>Learn More</Button>
+      </div>
+      <img
+        src="src/assets/home/desktop/image-hero-phone.png"
+        className={classes.phone}
+        alt=""
+      />
+    </section>
   );
 }

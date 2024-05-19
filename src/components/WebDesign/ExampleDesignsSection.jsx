7128ad41772216860bddc622977a22871cf587ad
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-19 11:06:06 +0200

    Add all examples

--- src/components/WebDesign/ExampleDesignsSection.jsx
+++ src/components/WebDesign/ExampleDesignsSection.jsx
@@ -6,6 +6,22 @@ export default function ExampleDesignsSection() {
       <SiteExample img="src/assets/desktop/image-express.jpg" title="Express">
         A multi-carrier shipping website for ecommerce businesses
       </SiteExample>
+      <SiteExample img="src/assets/desktop/image-transfer.jpg" title="Transfer">
+        Site for low-cost money transfers and sending money within seconds
+      </SiteExample>
+      <SiteExample img="src/assets/desktop/image-photon.jpg" title="Photon">
+        A state-of-the-art music player with high-resolution audio and DSP
+        effects
+      </SiteExample>
+      <SiteExample img="src/assets/desktop/image-builder.jpg" title="Builder">
+        Connects users with local contractors based on their location
+      </SiteExample>
+      <SiteExample img="src/assets/desktop/image-blogr.jpg" title="Blogr">
+        Blogr is a platform for creating an online blog or publication
+      </SiteExample>
+      <SiteExample img="src/assets/desktop/image-camp.jpg" title="Camp">
+        Get expert training in coding, data, design, and digital marketing
+      </SiteExample>
     </section>
   );
 }

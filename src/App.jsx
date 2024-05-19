bdf870a64d99f0b2d27adc46d72b9bfd889e7fb1
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-19 13:46:40 +0200

    ensure page is scrolled to top after path change

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,11 @@ function App() {
       </Route>
     )
   );
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/shared/Header.jsx";
 import Footer from "../components/shared/Footer.jsx";
+import ScrollToTop from "../components/ScrollToTop.js";
 
 export default function MainLayout() {
   return (
     <>
+      {
+        // ScrollToTop ensures that path change triggers window scroll to top
+      }
+      <ScrollToTop />
+
       <Header />
       <main>
         <Outlet />

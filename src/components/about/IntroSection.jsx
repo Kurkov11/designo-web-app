1923870d87e0215ee85c175c6c3def49affe3064
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-24 19:02:37 +0200

    prepare about section for tablet query

--- src/components/about/IntroSection.jsx
+++ src/components/about/IntroSection.jsx
@@ -2,21 +2,25 @@ import classes from "./IntroSection.module.css";
 
 export default function IntroSection() {
   return (
-    <section>
+    <section className={classes.section}>
       <div className={classes["intro-img"]} />
       <div className={classes.intro}>
-        <h1>About Us</h1>
-        {
-          // Unicode single quotes - prevent ide complaints
-        }
-        <p>
-          Founded in 2010, we are a creative agency that produces lasting
-          results for our clients. We{"\u2018"}ve partnered with many startups,
-          corporations, and nonprofits alike to craft designs that make real
-          impact. We{"\u2018"}re always looking forward to creating brands,
-          products, and digital experiences that connect with our clients
-          {"\u2018"} audiences.
-        </p>
+        <div className={classes["intro-bg"]} />
+        <div>
+          <h1>About Us</h1>
+          {
+            // Unicode single quotes - prevent ide complaints
+          }
+          <p>
+            Founded in 2010, we are a creative agency that produces lasting
+            results for our clients. We{"\u2018"}ve partnered with many
+            startups, corporations, and nonprofits alike to craft designs that
+            make real impact. We{"\u2018"}re always looking forward to creating
+            brands, products, and digital experiences that connect with our
+            clients
+            {"\u2018"} audiences.
+          </p>
+        </div>
       </div>
     </section>
   );

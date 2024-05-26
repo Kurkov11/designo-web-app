62eb29ad53ce705bfc6950b6745b14591b9b765d
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-26 09:23:39 +0200

    use phone img as a background not as an element

--- src/components/Home/IntroSection.jsx
+++ src/components/Home/IntroSection.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function IntroSection() {
   return (
     <section className={classes.intro}>
-      <div className={classes["intro-text"]}>
+      <div className={classes.text}>
         <h1>Award-winning custom designs and digital branding solutions</h1>
         <p>
           With over 10 years in the industry, we are experienced in creating
@@ -16,11 +16,6 @@ export default function IntroSection() {
           <Button>Learn More</Button>
         </Link>
       </div>
-      <img
-        src="/src/assets/home/desktop/image-hero-phone.png"
-        className={classes.phone}
-        alt=""
-      />
     </section>
   );
 }

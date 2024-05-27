bc211474a2577ea0284f87e11fc6f89798b4db49
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-27 16:57:20 +0200

    add background leafs for the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import CompanyValuesSection from "../components/home/CompanyValuesSection.jsx";
 import IntroSection from "../components/home/IntroSection.jsx";
 import ProjectCategoriesSection from "../components/design/project-categories/ProjectCategoriesSection.jsx";
-
+import classes from "./HomePage.module.css";
 export default function Home() {
   return (
-    <>
+    <div className={classes.background}>
       <IntroSection />
       <ProjectCategoriesSection />
       <CompanyValuesSection />
-    </>
+    </div>
   );
 }

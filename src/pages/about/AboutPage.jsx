34ecae1bfac6d325e263ade98e36cfc9556a72e2
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-29 17:10:45 +0200

    add background leafs to about page

--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -2,14 +2,15 @@ import IntroSection from "../../components/about/IntroSection";
 import RealDealSection from "../../components/about/RealDealSection";
 import WorldClassSection from "../../components/about/WorldClassSection";
 import LocationLinksSection from "../../components/location/LocationLinksSection";
+import classes from "./AboutPage.module.css";
 
 export default function About() {
   return (
-    <>
+    <div className={classes.background}>
       <IntroSection />
       <WorldClassSection />
       <LocationLinksSection />
       <RealDealSection />
-    </>
+    </div>
   );
 }

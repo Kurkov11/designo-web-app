dfb97e84c943753aeb021b79ece30db30432cf8b
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-30 08:48:48 +0200

    close hamburger menu on tablet query enter

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,14 @@
 import DarkeningOverlay from "./DarkeningOverlay";
 import HamburgerMenu from "./HamburgerMenu";
 import classes from "./Header.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+const TABLET_QUERY = 768;
 export default function Header() {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const [isTabletQuery, setIsTabletQuery] = useState(false);
+
   function handleHamburgerToggle() {
     setHamburgerOpen((wasOpen) => {
       // Disable or Enable Scroll
@@ -21,6 +24,22 @@ export default function Header() {
     // Enable scroll
     document.querySelector("body").style.overflow = "visible";
   }
+  function addResizeListener() {
+    window.addEventListener("resize", () => {
+      console.log(window.matchMedia(`(min-width: ${TABLET_QUERY}px)`).matches);
+      if (window.matchMedia(`(min-width: ${TABLET_QUERY}px)`).matches) {
+        setIsTabletQuery(true);
+        console.log("tablet");
+      } else {
+        setIsTabletQuery(false);
+        console.log("not tablet");
+      }
+    });
+  }
+
+  useEffect(() => addResizeListener, []);
+  useEffect(() => handleHamburgerClose, [isTabletQuery === true]);
+
   return (
     <header className={classes.wrapper}>
       <div className={classes.header}>

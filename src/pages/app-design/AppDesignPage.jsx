8346b6083aaa477403715f5d5577a94c86f4334c
Damian Kurkowicz <kurkovsw@icloud.com>
2024-05-31 19:18:32 +0200

    lower the desktop query

--- src/pages/app-design/AppDesignPage.jsx
+++ src/pages/app-design/AppDesignPage.jsx
@@ -1,11 +1,15 @@
 import ProjectCategoriesSection from "../../components/design/project-categories/ProjectCategoriesSection";
 import ExampleDesignsSection from "./AppExampleDesigns";
 import DesignIntro from "../../components/design/DesignIntro";
+import classes from "./AppDesignPage.module.css";
 
 export default function AppDesign() {
   return (
     <>
-      <DesignIntro title="App Design">
+      <DesignIntro
+        title="App Design"
+        classWithDesktopBg={classes["background-pattern"]}
+      >
         Our mobile designs bring intuitive digital solutions to your customers
         right at their fingertips.
       </DesignIntro>

afc121db0991cd427dcaaba2cb12c554a3012803
Damian Kurkowicz <kurkovsw@icloud.com>
2024-06-06 18:00:00 +0200

    Add framer motion hover states to dark background card

--- src/components/shared/UI/DarkBackgroundCard.jsx
+++ src/components/shared/UI/DarkBackgroundCard.jsx
@@ -1,12 +1,32 @@
 import classes from "./DarkBackgroundCard.module.css";
+import { motion } from "framer-motion";
 
 export default function DarkBackgroundCard({ children, className }) {
   const finalClassName = `${classes.card} ${className}`;
+
+  const cardVariants = {
+    rest: {},
+    hover: { backgroundSize: "102%" },
+  };
+  const overlayVariants = {
+    rest: {},
+    hover: {
+      backgroundColor: "rgba(231, 129, 107, 0.7)",
+    },
+  };
   return (
     <>
-      <div className={finalClassName}>
-        <div className={classes.overlay}>{children}</div>
-      </div>
+      <motion.div
+        initial="rest"
+        animate="rest"
+        whileHover="hover"
+        className={finalClassName}
+        variants={cardVariants}
+      >
+        <motion.div variants={overlayVariants} className={classes.overlay}>
+          {children}
+        </motion.div>
+      </motion.div>
     </>
   );
 }

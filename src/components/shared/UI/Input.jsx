f383e4ec5e3b50368bd2c16ec37cb88f6806e59d
Damian Kurkowicz <kurkovsw@icloud.com>
2024-06-08 12:47:41 +0200

    animate inputs while focused with framer motion

--- src/components/shared/UI/Input.jsx
+++ src/components/shared/UI/Input.jsx
@@ -1,14 +1,23 @@
 import classes from "./Input.module.css";
+import { motion } from "framer-motion";
 
 export default function Input({ textarea, ...props }) {
   if (textarea) {
     return (
-      <textarea
+      <motion.textarea
         className={`${classes.input} ${classes.textarea}`}
         type="text"
+        whileFocus={{ scale: 1.01 }}
         {...props}
       />
     );
   }
-  return <input className={classes.input} type="text" {...props} />;
+  return (
+    <motion.input
+      className={classes.input}
+      type="text"
+      {...props}
+      whileFocus={{ scale: 1.01 }}
+    />
+  );
 }

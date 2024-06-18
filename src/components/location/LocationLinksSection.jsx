3a96b6816319b5d35099acb3a34d5c71d8d719ce
Damian Kurkowicz <kurkovsw@icloud.com>
2024-06-18 20:57:56 +0200

    Fix images in location links section

--- src/components/location/LocationLinksSection.jsx
+++ src/components/location/LocationLinksSection.jsx
@@ -1,23 +1,26 @@
 import LocationLink from "./LocationLink";
 import classes from "./LocationLinksSection.module.css";
+import imgCanada from "/src/assets/images/shared/desktop/illustration-canada.svg";
+import imgAustralia from "/src/assets/images/shared/desktop/illustration-australia.svg";
+import imgUK from "/src/assets/images/shared/desktop/illustration-united-kingdom.svg";
 
 export default function LocationLinksSection() {
   return (
     <section className={classes.section}>
       <LocationLink
-        img="/src/assets/images/shared/desktop/illustration-canada.svg"
+        img={imgCanada}
         circleRotation="90deg"
         title="Canada"
         to="/locations/canada"
       />
       <LocationLink
-        img="/src/assets/images/shared/desktop/illustration-australia.svg"
+        img={imgAustralia}
         circleRotation="0deg"
         title="Australia"
         to="/locations/australia"
       />
       <LocationLink
-        img="/src/assets/images/shared/desktop/illustration-united-kingdom.svg"
+        img={imgUK}
         circleRotation="-90deg"
         title="United Kingdom"
         to="/locations/united-kingdom"

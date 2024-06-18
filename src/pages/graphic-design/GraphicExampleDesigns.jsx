916e52541e6d05a327d5f1434201113cfe89ced4
Damian Kurkowicz <kurkovsw@icloud.com>
2024-06-18 17:56:03 +0200

    Import images in graphic design examples section

--- src/pages/graphic-design/GraphicExampleDesigns.jsx
+++ src/pages/graphic-design/GraphicExampleDesigns.jsx
@@ -1,22 +1,25 @@
 import DesignExamplesSection from "../../components/design/DesignExamplesSection";
+import changeImg from "/src/assets/images/graphic-design/desktop/image-change.webp";
+import boxedWaterImg from "/src/assets/images/graphic-design/desktop/image-boxed-water.webp";
+import scienceImg from "/src/assets/images/graphic-design/desktop/image-science.webp";
 
 const GRAPHIC_DESIGN_EXAMPLES = [
   {
-    img: "/src/assets/images/graphic-design/desktop/image-change.webp",
+    img: changeImg,
     title: "Tim Brown",
     siteHref: "tim-brown",
     description: "A book cover designed for Tim Brown's new release, 'Change'",
     key: "graphic-design-example-tim-brown",
   },
   {
-    img: "/src/assets/images/graphic-design/desktop/image-boxed-water.webp",
+    img: boxedWaterImg,
     title: "Boxed Water",
     siteHref: "boxed-water",
     description: "A simple packaging concept made for Boxed Water",
     key: "graphic-design-boxed-water",
   },
   {
-    img: "/src/assets/images/graphic-design/desktop/image-science.webp",
+    img: scienceImg,
     title: "Science",
     siteHref: "science",
     description: "A poster made in collaboration with the Federal Art Project",
